4422d5b0c04e437b1fb113b7f2bc6765718ea761
kdha0727 <kdha0727@naver.com>
2021-09-08 19:40:15 +0900

    trying auto db-upgrade at db version changing

--- eyetuner_flask/config.py
+++ eyetuner_flask/config.py
@@ -2,7 +2,9 @@ import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite')
+DATABASE_DIR = os.path.join(BASE_DIR, 'db.sqlite')
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_DIR
 SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = 'ajserwhfdhkaudfhwkjeehw'
